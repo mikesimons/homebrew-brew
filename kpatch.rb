b2404d9d30ed57b88e5207597c22fb2e8cd2402f
Mike Simons <github@mikesimons.co.uk>
2019-02-14 10:31:43 +0100

    Adds kpatch

---
+++ kpatch.rb
@@ -0,0 +1,19 @@
+require "FileUtils"
+
+class Kpatch< Formula
+  homepage "https://github.com/mikesimons/kpatch"
+
+  $v = "v0.1.0"
+  url "https://github.com/mikesimons/envelope/releases/download/#{$v}/kpatch-#{$v}-darwin-amd64"
+  version $v
+  sha256 "715f2f6a58734b1f04f5d1cf3fa65060b1ff2884fa27373a1ba8e3cd40ba47b0"
+
+  def install
+    FileUtils.mv("kpatch-#{$v}-darwin-amd64", "kpatch")
+    bin.install "kpatch"
+  end
+
+  test do
+    system "#{bin}/kpatch", "--version"
+  end
+end
